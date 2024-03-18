a05b51332159f5e679cd703e4234e4a9ec2d9c89
Acchu <acchu1915@gmail.com>
2024-03-18 15:48:58 +0530

    Need Help
    finished
    traversing graph
    return true if found
    add a value to each of the array belonging to key

--- knightTravallis.js
+++ knightTravallis.js
@@ -15,6 +15,7 @@ const knightGraph = new Map();
     }
 }
 
+// Calculation for the Adjacency List for each Coordinate
 function knightPaths(knightCoordinates){
     const valueToAddOrSubtract = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]];
     const arrayKnightsPath = []
@@ -23,20 +24,74 @@ function knightPaths(knightCoordinates){
         const value1 = knightCoordinates[0] + a;
         const value2 = knightCoordinates[1] + b;
         const tempArray1 = [];
-        if(value1 > 0 && value1 < 8 && value2 > 0 && value2 < 8){
+        if(value1 > -1 && value1 < 8 && value2 > -1 && value2 < 8){
             tempArray1.push(value1);
             tempArray1.push(value2);
             arrayKnightsPath.push(tempArray1)
         }
     }
     knightGraph.set(knightCoordinates,arrayKnightsPath)
-    
-    
-
 }
 
-console.log(knightGraph)
+// console.log(knightGraph)
 
 
-// calculation function of what to put in the array which will be associated with the key
 //Traverse graph to check if traversed node === nodetoReach
+// Managed to push in distance but how would i get the shortest path by default the bottom will increment distance by +1 for every value being pushed into the queue. With counter i know the shortest required amount
+// but how to translate it to the mentioned output
+
+
+function breadthFirstTraversal(source,destination){
+    const hackQueue = [[source,0]];
+    while(hackQueue.length !== 0){
+        let arrayValues = hackQueue.shift();
+        let currentNode = arrayValues.shift();
+        let counter = arrayValues.shift();
+        console.log(`the current node is ${currentNode} and the counter is ${counter}`)
+        if(destinationPresent(currentNode,destination)){
+            return true;
+        }
+        for (const getKey of knightGraph.get(currentNode)){
+            let counterForPaths = counter;
+            const keyCoordinates = findCoordinatesOnMap(getKey)
+            hackQueue.push([keyCoordinates,counterForPaths+=1]);
+        }
+    }
+    return false;
+    
+}
+
+function destinationPresent(source,destination){
+    for (const getKey of knightGraph.get(source)){
+        const keyCoordinates = findCoordinatesOnMap(getKey)
+        if(keyCoordinates === destination){
+            return true;
+        }
+    }
+}
+
+
+// find coordinateKey which is the array on the map for reference to key
+function findCoordinatesOnMap(findKey){
+    const keyList = knightGraph.keys();
+    for(const coordinateKey of keyList){
+        const [index0, index1] = coordinateKey;
+        if(findKey[0] === index0 && findKey[1] === index1){
+            return coordinateKey
+        }
+    }
+    return 'Coordinate not present on Map';
+}
+
+const source = findCoordinatesOnMap([0,0]);
+const destination = findCoordinatesOnMap([1,6]);
+
+console.log(breadthFirstTraversal(source,destination))
+
+// const test = findCoordinatesOnMap([1,2]);
+// console.log(knightGraph.get(test));
+
+
+
+
+
