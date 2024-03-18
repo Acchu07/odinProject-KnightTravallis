b9f703f9706aeba0bb5654b6042ac055a85dc503
Acchu <acchu1915@gmail.com>
2024-03-18 12:03:30 +0530

    Completed Graph of Knight and KnightsPaths

--- knightTravallis.js
+++ knightTravallis.js
@@ -1,31 +1,42 @@
 //create a graph with its respective nodes 
 
 
+const tempStartLocation = 0
+const tempCountUp = 8
+
 const knightGraph = new Map();
 
-//Try to log out 0,0 till 7,7
-const arrayTest = []
- for(let i = 0; i < 8; i++){
-    for(let j = i; j<8;j++){
-        const tempMiniArray = [];
-        tempMiniArray.push(i)
-        tempMiniArray.push(j)
-        // console.log(tempMiniArray)
-        knightGraph.set(tempMiniArray,[]) // for every array it has a value of a empty array
-        // arrayTest.push(tempMiniArray)
+ for(let xAxis = tempStartLocation; xAxis < tempCountUp; xAxis++){
+    for(let yAxis = tempStartLocation; yAxis< tempCountUp;yAxis++){
+        const knightCoordinates = [];
+        knightCoordinates.push(xAxis)
+        knightCoordinates.push(yAxis)
+        knightPaths(knightCoordinates)
     }
 }
+
+function knightPaths(knightCoordinates){
+    const valueToAddOrSubtract = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]];
+    const arrayKnightsPath = []
+    for(const operations of valueToAddOrSubtract){
+        const [a , b] = operations
+        const value1 = knightCoordinates[0] + a;
+        const value2 = knightCoordinates[1] + b;
+        const tempArray1 = [];
+        if(value1 > 0 && value1 < 8 && value2 > 0 && value2 < 8){
+            tempArray1.push(value1);
+            tempArray1.push(value2);
+            arrayKnightsPath.push(tempArray1)
+        }
+    }
+    knightGraph.set(knightCoordinates,arrayKnightsPath)
+    
+    
+
+}
+
+console.log(knightGraph)
+
+
 // calculation function of what to put in the array which will be associated with the key
-
-//  knightGraph.forEach((value,key)=>{
-//     console.log(`key is ${key}, value is ${value} `)
-//  })
-
-const keyCheck = [0, 0]
-console.log(knightGraph.get(keyCheck));
-
-const arr = [];
-// console.log(arr)
-
-
 //Traverse graph to check if traversed node === nodetoReach
